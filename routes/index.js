52d74dbbefe81c246f1b3229e7967af9e9483975
Javier López <javierpitino@gmail.com>
2022-05-06 21:14:49 +0200

    Funcionando sin estilos

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,8 @@ router.get('/', (req, res, next) => res.render('index'));
 
 router.get("/movies", (req, res, next) => {
 
+
+    // 1. Buscar películas
     MovieModel.find()
     .then((response) => {
         res.render("movies.hbs", {
@@ -17,6 +19,26 @@ router.get("/movies", (req, res, next) => {
         console.log(err)
     })
 
+    // 2. Vista del usuario por cada peli
+    router.get("/movies/:movieId", async (req, res, next) => {
+        // 1. Obtener id de las pelis 
+        const { movieId } = req.params
+
+        try {
+            // 2. Buscar pelis por id
+            const respuesta = await MovieModel.findById( movieId )
+            console.log(respuesta)
+
+            // 3. Renderizar una vista el usuario con cada peli
+            res.render("movie-details.hbs", {
+                movieDetails: respuesta
+            })
+        }
+        catch(error) {
+            console.log(error)
+        }
+
+    })
 
 })
 
